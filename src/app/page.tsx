82037df222614b1026afab0806fd8ca41323c76f
Mateus Moura <mateuspragana@gmail.com>
2023-07-18 13:33:15 -0300

    feat: finalizando aula - Criando header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,21 @@ export default function Home() {
           priority={true}
         />
       </div>
+
       <h1 className={styles.title}>
         Sistema feito para você organizar
         <br /> seus estudos e tarefas
       </h1>
+
+      <div className={styles.infoContent}>
+        <section className={styles.box}>
+          <span>+12 posts</span>
+        </section>
+
+        <section className={styles.box}>
+          <span>+90 comentários</span>
+        </section>
+      </div>
     </main>
   );
 }
