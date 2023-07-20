f27d92a28c10de3aa71335c6f3a335cb33fe3b00
Mateus Moura <mateuspragana@gmail.com>
2023-07-20 17:31:01 -0300

    feat: inicio da aula cadastrar tarefas

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,77 +1,85 @@
-import { Metadata } from "next";
-import { getServerSession } from "next-auth";
-import { OPTIONS } from "../api/auth/[...nextauth]/route";
-import { redirect } from "next/navigation";
-import styles from "./styles.module.css";
+"use client";
+
 import { Textarea } from "@/components/textarea";
-import { FiShare2 } from "react-icons/fi";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import { useEffect, useState } from "react";
 import { FaTrash } from "react-icons/fa";
+import { FiShare2 } from "react-icons/fi";
+import styles from "./styles.module.css";
 
-export const metadata: Metadata = {
-  title: "Tarefas+ | Minhas tarefas",
-};
+export default function Dashboard() {
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect("/");
+    },
+  });
 
-export default async function Dashboard() {
-  const session = await getServerSession(OPTIONS);
+  useEffect(() => {
+    if (status === "authenticated") {
+      document.title = "Tarefas+ | Minhas Tarefas";
+    }
+  }, [status]);
 
-  if (!session) {
-    redirect("/");
-  }
+  const [input, setInput] = useState("");
 
   return (
-    <main className={styles.main}>
-      <section className={styles.content}>
-        <div className={styles.contentForm}>
-          <h1 className={styles.title}>Qual sua tarefa?</h1>
+    status === "authenticated" && (
+      <main className={styles.main}>
+        <section className={styles.content}>
+          <div className={styles.contentForm}>
+            <h1 className={styles.title}>Qual sua tarefa?</h1>
 
-          <form>
-            <Textarea placeholder="Digite qual sua tarefa..." />
-            <div className={styles.checkboxArea}>
-              <input type="checkbox" className={styles.checkbox} />
-              <label>Deixar tarefa pública?</label>
+            <form>
+              <Textarea placeholder="Digite qual sua tarefa..." />
+              <div className={styles.checkboxArea}>
+                <input type="checkbox" className={styles.checkbox} />
+                <label>Deixar tarefa pública?</label>
+              </div>
+              <button type="submit" className={styles.button}>
+                Registrar
+              </button>
+            </form>
+          </div>
+        </section>
+
+        <section className={styles.taskContainer}>
+          <h1>Minhas tarefas</h1>
+
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>PUBLICO</label>
+              <button className={styles.shareButton}>
+                <FiShare2 size={22} color="#3183ff" />
+              </button>
             </div>
-            <button type="submit" className={styles.button}>
-              Registrar
-            </button>
-          </form>
-        </div>
-      </section>
 
-      <section className={styles.taskContainer}>
-        <h1>Minhas tarefas</h1>
+            <div className={styles.taskContent}>
+              <p>Minha primeira tarefa de exemplo show demais</p>
+              <button className={styles.trashButton}>
+                <FaTrash size={24} color="#ea3140" />
+              </button>
+            </div>
+          </article>
 
-        <article className={styles.task}>
-          <div className={styles.tagContainer}>
-            <label className={styles.tag}>PUBLICO</label>
-            <button className={styles.shareButton}>
-              <FiShare2 size={22} color="#3183ff" />
-            </button>
-          </div>
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>PUBLICO</label>
+              <button className={styles.shareButton}>
+                <FiShare2 size={22} color="#3183ff" />
+              </button>
+            </div>
 
-          <div className={styles.taskContent}>
-            <p>Minha primeira tarefa de exemplo show demais</p>
-            <button className={styles.trashButton}>
-              <FaTrash size={24} color="#ea3140" />
-            </button>
-          </div>
-        </article>
-
-        <article className={styles.task}>
-          <div className={styles.tagContainer}>
-            <label className={styles.tag}>PUBLICO</label>
-            <button className={styles.shareButton}>
-              <FiShare2 size={22} color="#3183ff" />
-            </button>
-          </div>
-
-          <div className={styles.taskContent}>
-            <p>Minha primeira tarefa de exemplo show demais</p>
-            <button className={styles.trashButton}>
-              <FaTrash size={24} color="#ea3140" />
-            </button>
-          </div>
-        </article>
-      </section>
-    </main>
+            <div className={styles.taskContent}>
+              <p>Minha primeira tarefa de exemplo show demais</p>
+              <button className={styles.trashButton}>
+                <FaTrash size={24} color="#ea3140" />
+              </button>
+            </div>
+          </article>
+        </section>
+      </main>
+    )
   );
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,44 +1,51 @@
 "use client";
 
-import Link from "next/link";
-import styles from "./styles.module.css";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { redirect, usePathname } from "next/navigation";
+import styles from "./styles.module.css";
 
 export function Header() {
-  const { data: session, status } = useSession();
+  const { data: session, status, data } = useSession();
+  const pathName = usePathname();
 
-  return (
-    <>
-      <header className={styles.header}>
-        <section className={styles.content}>
-          <nav className={styles.nav}>
-            <Link href="/">
-              <h1 className={styles.logo}>
-                Tarefas <span>+</span>
-              </h1>
+  return pathName !== "/" && status === "loading" ? (
+    <span>Processando...</span>
+  ) : (
+    <header className={styles.header}>
+      <section className={styles.content}>
+        <nav className={styles.nav}>
+          <Link href="/">
+            <h1 className={styles.logo}>
+              Tarefas <span>+</span>
+            </h1>
+          </Link>
+          {session?.user && (
+            <Link className={styles.link} href="/dashboard">
+              Meu Painel
             </Link>
-            {session?.user && (
-              <Link className={styles.link} href="/dashboard">
-                Meu Painel
-              </Link>
-            )}
-          </nav>
-          {status === "loading" ? (
-            <></>
-          ) : session ? (
-            <button className={styles.loginButton} onClick={() => signOut()}>
-              Olá {session?.user?.name}
-            </button>
-          ) : (
-            <button
-              className={styles.loginButton}
-              onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
-            >
-              Acessar
-            </button>
           )}
-        </section>
-      </header>
-    </>
+        </nav>
+        {session ? (
+          <button
+            className={styles.loginButton}
+            onClick={() =>
+              signOut({
+                callbackUrl: "/",
+              })
+            }
+          >
+            Olá {session?.user?.name}
+          </button>
+        ) : (
+          <button
+            className={styles.loginButton}
+            onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
+          >
+            Acessar
+          </button>
+        )}
+      </section>
+    </header>
   );
 }
